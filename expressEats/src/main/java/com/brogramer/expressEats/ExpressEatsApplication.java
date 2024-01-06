ecdedb95ea77f873909277c8ab300f9af39a5670
LekeMarkaj <markaj.leka@gmail.com>
2024-01-06 22:53:18 +0100

    0

---
+++ expressEats/src/main/java/com/brogramer/expressEats/ExpressEatsApplication.java
@@ -0,0 +1,13 @@
+package com.brogramer.expressEats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressEatsApplication.class, args);
+	}
+
+}
---
+++ expressEats/src/test/java/com/brogramer/expressEats/ExpressEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brogramer.expressEats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
