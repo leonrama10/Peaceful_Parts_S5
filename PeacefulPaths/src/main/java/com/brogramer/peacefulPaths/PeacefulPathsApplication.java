dda1083ddf10f10771a4b1a8706cd7b2a37a92f3
LekeMarkaj <markaj.leka@gmail.com>
2024-01-07 15:36:23 +0100

    0

---
+++ PeacefulPaths/src/main/java/com/brogramer/peacefulPaths/PeacefulPathsApplication.java
@@ -0,0 +1,13 @@
+package com.brogramer.peacefulPaths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeacefulPathsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeacefulPathsApplication.class, args);
+	}
+
+}
---
+++ PeacefulPaths/src/test/java/com/brogramer/peacefulPaths/PeacefulPathsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brogramer.peacefulPaths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeacefulPathsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
